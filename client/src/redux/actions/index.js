707b55d76d0d5e4a6853be0a3527af5244fbba01
RepasoHenry <devWarrior7@protonmail.com>
2022-06-05 22:18:29 -0500

    Cambios en Carrito y Actions

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -365,20 +365,21 @@ export function updateProductos(product) {
 }
 
 export const paymentMP = async(items,user, navigate,emptyCart) =>{
-  
   const carrito = []
         items.map((i)=>{
             carrito.push({
-                idUser: user.idUser,
+                idUser: user.id,
                 idProduct:i.idProduct,
                 quantity:i.quantity
             })
         })
+  const token = localStorage.getItem('token')
   const response = await fetch("https://barber-app-henry.herokuapp.com/api/purchaseOrder", {
     method: "POST",
     body: JSON.stringify(carrito),
     headers: {
       "Content-Type": "application/json",
+      "x-token": token
     },
   });
   const json = await response.json();
