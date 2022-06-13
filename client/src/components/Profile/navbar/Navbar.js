1b370e0cbf07fb4e4f1d6335af3d1b1b989646ad
david cuspoca <cuspoca3d@gmail.com>
2022-06-12 19:54:31 -0600

    se puso exact en la ruta de salir del navbar

--- client/src/components/Profile/navbar/Navbar.js
+++ client/src/components/Profile/navbar/Navbar.js
@@ -64,6 +64,7 @@ export const Navbar = () => {
         </NavLink>
 
         <NavLink
+          exact
           className="list-group-item pointer"
           activeClassName="bg-warning"
           to="/"
